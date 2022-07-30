fd8f40c685f5d1c229f22752a7fbfc731bc8c7be
Gowtham <108211211+Codepanda008@users.noreply.github.com>
2022-07-30 16:35:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let obj=[
+    {
+        "basics": {
+          "name": "Gowtham Mukil B",
+          "DOB": "23-04-1999",
+          "Age": "23",
+          "email": "gowtham@gmail.com",
+          "phone": "9080079945",
+          "degree": "B.E Computer Science",
+          "website": "https://gist.github.com/Codepanda008",
+          "summary": "I am able to handel Multiple Tasks.",
+          "location":{ 
+            "address": "VOC Street",
+            "postalCode": "625603",
+            "city": "Theni",
+            "countryCode": "India",
+            "region": "Tamil Nadu"
+        },
+          
+      }
+    }
+      
+]
+console.log(obj)
